c2cbdd48bfb7644c33fa7adf493904d9cae9456e
Reza Shirazian <reza.shirazian@gmail.com>
2017-07-25 13:29:33 -0700

    initial commit

---
+++ SeeFood/FrameExtractor.swift
@@ -0,0 +1,108 @@
+//
+//  FrameExtractor.swift
+//  Created by Bobo on 29/12/2016.
+//
+
+import UIKit
+import AVFoundation
+
+protocol FrameExtractorDelegate: class {
+  func captured(image: UIImage)
+}
+
+class FrameExtractor: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+  
+  private var position = AVCaptureDevice.Position.back
+  private let quality = AVCaptureSession.Preset.medium
+  
+  private var permissionGranted = false
+  private let sessionQueue = DispatchQueue(label: "session queue")
+  private let captureSession = AVCaptureSession()
+  private let context = CIContext()
+  
+  weak var delegate: FrameExtractorDelegate?
+  
+  override init() {
+    super.init()
+    checkPermission()
+    sessionQueue.async { [unowned self] in
+      self.configureSession()
+      self.captureSession.startRunning()
+    }
+  }
+  
+  public func flipCamera() {
+    sessionQueue.async { [unowned self] in
+      self.captureSession.beginConfiguration()
+      guard let currentCaptureInput = self.captureSession.inputs.first as? AVCaptureInput else { return }
+      self.captureSession.removeInput(currentCaptureInput)
+      guard let currentCaptureOutput = self.captureSession.outputs.first as? AVCaptureOutput else { return }
+      self.captureSession.removeOutput(currentCaptureOutput)
+      self.position = self.position == .front ? .back : .front
+      self.configureSession()
+      self.captureSession.commitConfiguration()
+    }
+  }
+  
+  // MARK: AVSession configuration
+  private func checkPermission() {
+    switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+    case .authorized:
+      permissionGranted = true
+    case .notDetermined:
+      requestPermission()
+    default:
+      permissionGranted = false
+    }
+  }
+  
+  private func requestPermission() {
+    sessionQueue.suspend()
+    AVCaptureDevice.requestAccess(for: AVMediaType.video) { [unowned self] granted in
+      self.permissionGranted = granted
+      self.sessionQueue.resume()
+    }
+  }
+  
+  private func configureSession() {
+    guard permissionGranted else { return }
+    captureSession.sessionPreset = quality
+    guard let captureDevice = selectCaptureDevice() else { return }
+    guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+    guard captureSession.canAddInput(captureDeviceInput) else { return }
+    captureSession.addInput(captureDeviceInput)
+    let videoOutput = AVCaptureVideoDataOutput()
+    videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+    guard captureSession.canAddOutput(videoOutput) else { return }
+    captureSession.addOutput(videoOutput)
+    guard let connection = videoOutput.connection(with: AVFoundation.AVMediaType.video) else { return }
+    guard connection.isVideoOrientationSupported else { return }
+    guard connection.isVideoMirroringSupported else { return }
+    connection.videoOrientation = .portrait
+    connection.isVideoMirrored = position == .front
+  }
+  
+  private func selectCaptureDevice() -> AVCaptureDevice? {
+    return AVCaptureDevice.devices().filter {
+      ($0 as AnyObject).hasMediaType(AVMediaType.video) &&
+        ($0 as AnyObject).position == position
+      }.first as? AVCaptureDevice
+  }
+  
+  // MARK: Sample buffer to UIImage conversion
+  private func imageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
+    guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+    let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+    guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
+    return UIImage(cgImage: cgImage)
+  }
+  
+  // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
+  func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    guard let uiImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
+    DispatchQueue.main.async { [unowned self] in
+      self.delegate?.captured(image: uiImage)
+    }
+  }
+}
+
---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Reza Shirazian on 7/23/17.
+//  Copyright © 2017 Reza Shirazian. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+import AVFoundation
+
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate, FrameExtractorDelegate {
+ 
+  var frameExtractor: FrameExtractor!
+  
+  @IBOutlet weak var previewImage: UIImageView!
+  @IBOutlet weak var iSee: UILabel!
+
+  var settingImage = false
+  
+  var currentImage: CIImage? {
+    didSet {
+      if let image = currentImage{
+        self.detectScene(image: image)
+      }
+    }
+  }
+  
+  @IBOutlet weak var cameraLayer: UIView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    frameExtractor = FrameExtractor()
+    frameExtractor.delegate = self
+  }
+  
+  func captured(image: UIImage) {
+    self.previewImage.image = image
+    if let cgImage = image.cgImage, !settingImage {
+      settingImage = true
+      DispatchQueue.global(qos: .userInteractive).async {[unowned self] in
+        self.currentImage = CIImage(cgImage: cgImage)
+      }
+    }
+  }
+
+  func addEmoji(id: String) -> String {
+    switch id {
+    case "pizza":
+      return "🍕"
+    case "hot dog":
+      return "🌭"
+    case "chicken wings":
+      return "🍗"
+    case "french fries":
+      return "🍟"
+    case "sushi":
+      return "🍣"
+    case "chocolate cake":
+      return "🍫🍰"
+    case "donut":
+      return "🍩"
+    case "spaghetti bolognese":
+      return "🍝"
+    case "caesar salad":
+      return "🥗"
+    case "macaroni and cheese":
+      return "🧀"
+    default:
+      return ""
+    }
+  }
+  func detectScene(image: CIImage) {
+    guard let model = try? VNCoreMLModel(for: food().model) else {
+      fatalError()
+    }
+    // Create a Vision request with completion handler
+    let request = VNCoreMLRequest(model: model) { [unowned self] request, error in
+      guard let results = request.results as? [VNClassificationObservation],
+        let _ = results.first else {
+          self.settingImage = false
+          return
+      }
+      
+      DispatchQueue.main.async { [unowned self] in
+        if let first = results.first {
+           if Int(first.confidence * 100) > 1 {
+            self.iSee.text = "I see \(first.identifier) \(self.addEmoji(id: first.identifier))"
+            self.settingImage = false
+          }
+        }
+//        results.forEach({ (result) in
+//          if Int(result.confidence * 100) > 1 {
+//            self.settingImage = false
+//            print("\(Int(result.confidence * 100))% it's \(result.identifier) ")
+//          }
+//        })
+       // print("********************************")
+        
+      }
+    }
+    let handler = VNImageRequestHandler(ciImage: image)
+    DispatchQueue.global(qos: .userInteractive).async {
+      do {
+        try handler.perform([request])
+      } catch {
+        print(error)
+      }
+    }
+  }
+}
+
